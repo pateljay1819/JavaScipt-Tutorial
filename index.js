bacbc389b0e54d999a98f312962875394f4e0ab9
Jay Patel <145035699+pateljay1819@users.noreply.github.com>
2024-08-21 17:47:12 +0530

    addd via file

---
+++ index.js
@@ -0,0 +1,40 @@
+// 1.ways to use print hello world
+
+
+console.log("Hello World");
+
+// 2. js console api
+console.warn("This is warning");
+console.error("This is error");
+
+//3.variables-to store data
+var a = 34;
+var b = 26;
+console.log(a + b);
+
+// 4.Datatypes in java script
+// numbers
+var num1 = 45;
+var num2 = 56.78;
+
+// string
+var str1 = "this is string"
+var str2 = "this is also string"
+
+//objects
+var marks = {
+    jay: 99,
+    butesh: 70,
+    jayant : 81,
+}
+
+//boolens
+var a = true;
+var b = false;
+ console.log(a,b);
+
+ var und = undefined;
+ console.log(und);
+
+ var n = null;
+ console.log(n);
