27ff89682776a5f4a8fb2ca38e59752174c26f1d
Jay Patel <145035699+pateljay1819@users.noreply.github.com>
2024-08-21 17:53:51 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,18 @@
+
+let firstName = 'Jay'    
+let lastName = 'Patel'      
+let country = 'India'       
+let city = 'Ahmedabad'       
+let age = 22                
+
+
+
+const gravity = 9.81        
+const boilingPoint = 100     
+const PI = 3.14             
+
+//
+
+let name = 'Jay',     
+  job = 'web devloper',
+  live = 'India'
