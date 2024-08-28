b29cc201a410716bf997c3b1b71f983c7f850e1e
jay patel <jaykpatel1863@gmail.com>
2024-08-28 17:31:25 +0530

    loops file added

---
+++ loop.js
@@ -0,0 +1,25 @@
+//for loop
+{
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+  }
+
+}
+  //while loop
+  {
+  let i = 0;
+while (i < 5) {
+  console.log("Iteration number: " + i);
+  i++;
+}
+  }
+
+//dowhile loop
+{
+let i = 0;
+do {
+  console.log("Iteration number: " + i);
+  i++;
+} while (i < 5);
+
+}
