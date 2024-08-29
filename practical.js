9c4c24b92e813c4c01768beab6b50c69f1bd6bdf
jay patel <jaykpatel1863@gmail.com>
2024-08-29 15:45:07 +0530

    file added

---
+++ practical.js
@@ -0,0 +1,8 @@
+let gameNum = 25;
+let userNum = prompt("Guess the game number : ");
+
+while (userNum != gameNum) {
+    userNum = prompt("You entered wrong number. Guess again :");
+}
+
+console.log("congratulations, you enterd the right number");
