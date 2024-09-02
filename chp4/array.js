5e151d566cc19e035c1bed91b1e0d4d0e637803e
jay patel <jaykpatel1863@gmail.com>
2024-09-02 17:28:14 +0530

    array done

--- chp4/array.js
+++ chp4/array.js
@@ -9,7 +9,7 @@ let avg = sum/marks.length;
 console.log(`avg marks of the class = ${avg}`);
 
 
-//let student_name =["jay","uday","butesh","jayant","dhruv","dev"];
+let student_name =["jay","uday","butesh","jayant","dhruv","dev"];
 //for loop
 //for(let i = 0; i<student_name.length; i++) {
   //  console.log(student_name[i]);
@@ -19,3 +19,45 @@ console.log(`avg marks of the class = ${avg}`);
 //for(let name of student_name){
   //  console.log(student_name);
 //}
+console.log(student_name);
+console.log(student_name.toString());
+
+
+
+
+
+
+//let marvelheroes = ["thor","spiderman","ironman"]
+//marvelheroes.unshift("antman"); //add to start
+
+//let val = marvelheroes.shift(); //delted to start
+//console.log("deleted" , val);
+//console.log(marvelheroes);
+
+
+
+//let dcheroes = ["superman","batman"];
+
+//console.log(dcheroes);
+
+//let heroes = marvelheroes.concat(dcheroes); // combine two arrays
+//console.log(heroes);
+//console.log(heroes.toString());
+
+
+
+let SportBikes =["r15","ktm","kawasaki ninja","truimph","apache"]
+console.log(SportBikes.slice(1,3));   //return a piece of array
+
+
+let arr = [1,2,3,4,,5,6,7,8];
+//arr.splice(2,2,101,102);
+
+//add element
+//arr.splice(2,0,101);
+
+//delete element
+//arr.splice(3,1);
+
+//replace element
+arr.splice(3, 1,101);
